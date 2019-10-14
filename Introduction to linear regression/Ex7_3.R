6c217d5cea3940bec15b3016eae17db8f98a8833
thesaltree <46742197+thesaltree@users.noreply.github.com>
2019-10-14 21:42:17 +0530

    Create Ex7_3.R

---
+++ Introduction to linear regression/Ex7_3.R
@@ -0,0 +1,13 @@
+##Section: 7.1.3   Page No.:336
+
+## Data= Fit + Residual
+x_data=77.0
+y_data=85.3
+y_estimate=41+0.59*x_data
+y_estimate
+
+residual=y_data-y_estimate
+residual
+
+cat("Residual of the observation is",residual,"and is very close to the visual estimate,-1.")
+
